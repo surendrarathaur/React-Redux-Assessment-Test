9f02ced7db82bdae6b91d82359e2a02c84b1d651
surendra rathor <surendrarathor87@gmail.com>
2019-06-10 12:56:57 +0530

    first code

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Banner = ({ appName, token }) => {
+  if (token) {
+    return null;
+  }
+  return (
+    <div className="banner">
+      <div className="container">
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
